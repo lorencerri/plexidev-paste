a77cef02e1a69df42f826d18dd9278535980dd04
Loren Cerri <lorencerri@gmail.com>
2022-04-14 23:35:02 -0400

    Add TypeScript Types

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+declare module "plexidev-paste" {
+    export interface Options {
+        title?: string | null;
+        language?: string | null;
+    }
+
+    /**
+     *
+     * @param content Any string as text to be uploaded.
+     * @param options An object of options to be passed to the paste server.
+     */
+    function post(content: string, options?: Options): any;
+
+    /**
+     *
+     * @param id The id of the paste to be retrieved.
+     */
+    function get(id: string): any;
+}
